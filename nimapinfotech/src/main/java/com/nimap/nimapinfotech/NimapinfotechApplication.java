de50f7abd15989df2f7a58b2e441773d2d09051b
Kavya <Kavya@LAPTOP-J7I8TLJ3>
2022-10-17 11:41:01 +0530

    First Commit

---
+++ nimapinfotech/src/main/java/com/nimap/nimapinfotech/NimapinfotechApplication.java
@@ -0,0 +1,14 @@
+package com.nimap.nimapinfotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NimapinfotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimapinfotechApplication.class, args);
+	}
+
+}
